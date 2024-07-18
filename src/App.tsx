dd108a8e35fb765f9b3908ebd857ae51ff074c7d
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-07-18 12:27:22 +0200

    project init & folder structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable eol-last */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable prettier/prettier */
+import { Text, View } from 'react-native';
+
+
+export const App = () => {
+  return(
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
