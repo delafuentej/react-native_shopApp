4b1f0b28f23559c6fce92e4ee041796a5a06789d
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-07-23 19:17:33 +0200

    product update function

---
+++ src/actions/products/update-create-product.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prettier/prettier */
+import { shopApi } from "../../config/api/shopApi";
+
+
+
+export const updateCreateProduct = (product: Partial<Product>)=> {
+    
+
+}
